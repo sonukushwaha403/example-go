0d7885829d33d5547e3557aeb3861d0e288a9c6a
sonu kumar kushwaha <sksonukushwaha403@gmail.com>
2020-11-26 21:57:20 +0530

    You're doing great

---
+++ 005_type_data_type_formatting.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	f"fmt"
+)
+//formatting the variable
+//ie finding the type of variable declared
+//golang being the lang of static type 
+//where as java script is a dynamic type 
+var x = 4.0
+
+
+func main() {
+	f.Println(x)
+	f.Printf("%T",x)
+}
+
