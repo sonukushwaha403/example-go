eb2dafe8d8aa736952f4ef45bed2f3440004af21
sonu kumar kushwaha <sksonukushwaha403@gmail.com>
2020-11-28 19:17:32 +0530

    you are doing good

---
+++ 009_constant.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+type( 
+kushwaha int
+sonu int
+)
+var(
+a sonu=34
+b kushwaha=35
+)
+const(
+s=88
+k=96
+)
+
+func main() {
+	fmt.Printf("%T\n%T\n",a,b)
+	fmt.Print(s,k)
+}
